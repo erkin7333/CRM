deada66bba9f86ba234dd036aeecb1076add9dbd
erkin7333 <erkinnajimov90@gmail.com>
2023-01-03 13:18:48 +0500

    CRUD....

--- crm_core/urls.py
+++ crm_core/urls.py
@@ -4,5 +4,13 @@ from .views import *
 app_name = 'crm_core'
 
 urlpatterns = [
-    path('add-lead/', add_lead, name='add_lead')
+    path('add-lead/', add_lead, name='add_lead'),
+
+    path('leads/', lead_list, name='leads'),
+
+    path('lead-detail/<int:pk>/', lead_deteil, name='lead_detail'),
+
+    path('lead-edit/<int:pk>/', edit_lead, name='edit'),
+
+    path('<int:pk>/', lead_delete, name='delete')
 ]
--- crm_core/views.py
+++ crm_core/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import AddLeadForm
+from .models import Lead
 
 
+@login_required
+def lead_list(request):
+    """Foydalanuvchilarni bazadan malumotlarin olish funksiyasi"""
+
+    leads = Lead.objects.filter(created_by=request.user)
+    context = {
+        'leads': leads
+    }
+    return render(request, 'crm_core/lead-list.html', context=context)
+
+
+@login_required
+def lead_deteil(request, pk):
+    """Foydalanuvchini batafsil malumotlarini ko'rish uchun funksiya"""
+
+    lead_d = get_object_or_404(Lead, created_by=request.user, id=pk)
+    context = {
+        'lead_d': lead_d
+    }
+    return render(request, 'crm_core/lead-deteil.html', context=context)
+
+
+@login_required
+def lead_delete(request, pk):
+    """Foydalanuvchini o'chirish uchun funksiya"""
+
+    lead = get_object_or_404(Lead, created_by=request.user, id=pk)
+    lead.delete()
+    messages.success(request, "Malumot o'chirildi")
+    return redirect('crm_core:leads')
+
+
+@login_required
+def edit_lead(request, pk):
+    """Foydalanuvchini yangilash uchun funksiya"""
+
+    lead = get_object_or_404(Lead, created_by=request.user, id=pk)
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Foydalanuvchi malumotlari o'zgartirildi!")
+            return redirect('crm_core:leads')
+    else:
+        form = AddLeadForm(instance=lead)
+        context = {'form': form}
+    return render(request, 'crm_core/lead-edit.html', context=context)
+
+@login_required
 def add_lead(request):
-    """............"""
-    return render(request, 'crm_core/add-lead.html')
+    """Foydalanuvchi qo'shish uchun funksiya"""
+
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+            messages.success(request, "Foydalanuvchi qo'shildi")
+            return redirect('crm_core:leads')
+    else:
+        form = AddLeadForm()
+    return render(request, 'crm_core/add-lead.html', {'form': form})
