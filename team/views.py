a5521ccbdb3d60d220876ed8d5d70c60df6ef6f6
erkin7333 <erkinnajimov90@gmail.com>
2023-01-04 13:58:19 +0500

    .........

--- team/views.py
+++ team/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Team
+from .forms import TeamForm
 
 
 @login_required
@@ -8,8 +9,14 @@ def edit_team(request, pk):
     """Jamoa malumotini o'zgartirish funksiyasi"""
 
     team = get_object_or_404(Team, created_by=request.user, id=pk)
-    context = {
-        'team': team
-    }
-    return render(request, 'team/edit-team.html', context=context)
-
+    form = TeamForm(request.POST, instance=team)  # define the form variable here
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('account:myaccount')
+    else:
+        form = TeamForm(instance=team)
+    return render(request, 'team/edit-team.html', {
+        'form': form,
+        'team': team,
+    })
